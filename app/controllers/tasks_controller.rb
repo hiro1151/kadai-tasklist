4d8c16d5178d7e87cb1b8ec7171f6362cb53461f
hiro1151 <hiro.gitcut77@gmail.com>
2017-08-22 08:10:28 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
-  before_action :set_message,only: [:show,:edit,:update,:destroy]
+  before_action :set_task,only: [:show,:edit,:update,:destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-   end
+  end
 
   def new
      @task = Task.new
